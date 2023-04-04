0ea19726da2ddb11e891f1ff1e9274f6b734f980
WeijieChen2017 <weijiechen1994@gmail.com>
2023-04-04 17:24:12 -0500

    Good morning!

--- data_preprocess.py
+++ data_preprocess.py
@@ -3,35 +3,59 @@ import numpy as np
 import os
 import glob
 
-NAC_list = sorted(glob.glob("./data/Iman/PET_256_MAX_MIN_2_Not_Normalized/*.nii"))
-CT_list = sorted(glob.glob("./data/Iman/CT_256_MAX_MIN_2_Not_Normalized/*.nii"))
+MR_list = sorted(glob.glob("./data/Task1/*/*/mr.nii"))
+CT_list = sorted(glob.glob("./data/Task1/*/*/ct.nii"))
 
-for folder_path in ["./data/NAC_wb_norm/", "./data/CT_wb_norm/"]:
+for folder_path in ["./data/t1_mr_norm/", "./data/t1_ct_norm/"]:
     if not os.path.exists(folder_path):
         os.mkdir(folder_path)
 
-for file_path in NAC_list:
+MR_range_min = []
+MR_range_max = []
+MR_range_q999 = []
+
+CT_range_min = []
+CT_range_max = []
+CT_range_q999 = []
+
+for file_path in MR_list:
     file_name = os.path.basename(file_path)
     print(file_name, end=" -> ")
-    NAC_file = nib.load(file_path)
-    NAC_data = NAC_file.get_fdata()
-    print(NAC_data.shape)
+    MR_file = nib.load(file_path)
+    MR_data = MR_file.get_fdata()
+    data_min = np.amin(MR_data)
+    data_max = np.amax(MR_data)
+    data_q999 = np.percentile(MR_data, 99.9)
+    print(MR_data.shape, data_min, data_max, data_q999)
+    MR_range_min.append(data_min)
+    MR_range_max.append(data_max)
+    MR_range_q999.append(data_q999)
     # print(np.percentile(NAC_data, 99.9))
     # NAC_data = np.resize(NAC_data, (256, 256, NAC_data.shape[2]))
-    NAC_data = NAC_data / 10000
-    new_NAC_file = nib.Nifti1Image(NAC_data, NAC_file.affine, NAC_file.header)
-    new_save_name = "./data/NAC_wb_norm/" + file_name + ".gz"
-    nib.save(new_NAC_file, new_save_name)
+    # NAC_data = NAC_data / 10000
+    # new_NAC_file = nib.Nifti1Image(NAC_data, NAC_file.affine, NAC_file.header)
+    # new_save_name = "./data/NAC_wb_norm/" + file_name + ".gz"
+    # nib.save(new_NAC_file, new_save_name)
+
+np.save("./data/t1_mr_norm/MR_range.npy", [MR_range_min, MR_range_max, MR_range_q999])
 
 for file_path in CT_list:
     file_name = os.path.basename(file_path)
     print(file_name, end=" -> ")
     CT_file = nib.load(file_path)
     CT_data = CT_file.get_fdata()
-    print(CT_data.shape)
+    data_min = np.amin(CT_data)
+    data_max = np.amax(CT_data)
+    data_q999 = np.percentile(CT_data, 99.9)
+    print(CT_data.shape, data_min, data_max, data_q999)
+    CT_range_min.append(data_min)
+    CT_range_max.append(data_max)
+    CT_range_q999.append(data_q999)
     # print(np.amax(CT_data), np.amin(CT_data)) [2000.0, 0.0]
     # CT_data = np.resize(CT_data, (256, 256, CT_data.shape[2]))
-    CT_data = CT_data / 2000
-    new_CT_file = nib.Nifti1Image(CT_data, CT_file.affine, CT_file.header)
-    new_save_name = "./data/CT_wb_norm/" + file_name + ".gz"
-    nib.save(new_CT_file, new_save_name)
+    # CT_data = CT_data / 2000
+    # new_CT_file = nib.Nifti1Image(CT_data, CT_file.affine, CT_file.header)
+    # new_save_name = "./data/CT_wb_norm/" + file_name + ".gz"
+    # nib.save(new_CT_file, new_save_name)
+
+np.save("./data/t1_ct_norm/CT_range.npy", [CT_range_min, CT_range_max, CT_range_q999])
