598d591af96a4d79eb670bc7dc70b9538bd3e34d
WeijieChen2017 <weijiechen1994@gmail.com>
2023-04-04 18:49:51 -0500

    Good morning!

--- data_preprocess.py
+++ data_preprocess.py
@@ -18,6 +18,9 @@ for folder_path in ["./data/t1_mr_norm/", "./data/t1_ct_norm/"]:
 # CT_range_max = []
 # CT_range_q999 = []
 
+largest_z_MR = 0
+largest_z_CT = 0
+
 for file_path in MR_list:
 
     path_parts = file_path.split(os.sep)
@@ -29,6 +32,9 @@ for file_path in MR_list:
     # print(file_name, end=" -> ")
     MR_file = nib.load(file_path)
     MR_data = MR_file.get_fdata()
+    print(MR_data.shape)
+    if MR_data.shape[2] > largest_z_MR:
+        largest_z_MR = MR_data.shape[2]
     # MR_mask = nib.load(file_path.replace("mr.nii.gz", "mask.nii.gz")).get_fdata()
     # MR_data = MR_data[MR_mask.astype(bool)]
     # data_min = np.amin(MR_data)
@@ -40,10 +46,10 @@ for file_path in MR_list:
     # MR_range_q999.append(data_q999)
     # print(np.percentile(NAC_data, 99.9))
     # NAC_data = np.resize(NAC_data, (256, 256, NAC_data.shape[2]))
-    MR_data = MR_data / 3000
-    new_MR_file = nib.Nifti1Image(MR_data, MR_file.affine, MR_file.header)
-    new_save_name = "./data/t1_mr_norm/" + new_filename
-    nib.save(new_MR_file, new_save_name)
+    # MR_data = MR_data / 3000
+    # new_MR_file = nib.Nifti1Image(MR_data, MR_file.affine, MR_file.header)
+    # new_save_name = "./data/t1_mr_norm/" + new_filename
+    # nib.save(new_MR_file, new_save_name)
 
 # np.save("./data/t1_mr_norm/MR_range.npy", [MR_range_min, MR_range_max, MR_range_q999])
 
@@ -58,6 +64,9 @@ for file_path in CT_list:
     # print(file_name, end=" -> ")
     CT_file = nib.load(file_path)
     CT_data = CT_file.get_fdata()
+    print(CT_data.shape)
+    if CT_data.shape[2] > largest_z_CT:
+        largest_z_CT = CT_data.shape[2]
     # CT_mask = nib.load(file_path.replace("ct.nii.gz", "mask.nii.gz")).get_fdata()
     # CT_data = CT_data[CT_mask.astype(bool)]
     # data_min = np.amin(CT_data)
@@ -69,10 +78,12 @@ for file_path in CT_list:
     # CT_range_q999.append(data_q999)
     # print(np.amax(CT_data), np.amin(CT_data)) [2000.0, 0.0]
     # CT_data = np.resize(CT_data, (256, 256, CT_data.shape[2]))
-    CT_data = (CT_data + 1024) / (3000+1024)
-    new_CT_file = nib.Nifti1Image(CT_data, CT_file.affine, CT_file.header)
-    new_save_name = "./data/t1_ct_norm/" + new_filename
-    nib.save(new_CT_file, new_save_name)
-    print(new_filename, "saved")
+    # CT_data = (CT_data + 1024) / (3000+1024)
+    # new_CT_file = nib.Nifti1Image(CT_data, CT_file.affine, CT_file.header)
+    # new_save_name = "./data/t1_ct_norm/" + new_filename
+    # nib.save(new_CT_file, new_save_name)
+    # print(new_filename, "saved")
 
 # np.save("./data/t1_ct_norm/CT_range.npy", [CT_range_min, CT_range_max, CT_range_q999])
+
+print(largest_z_MR, largest_z_CT)
