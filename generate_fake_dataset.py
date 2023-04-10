687a3428a70f78dda392f8e60f85b09e6cca3b68
WeijieChen2017 <weijiechen1994@gmail.com>
2023-04-10 00:05:27 -0500

    Good morning!

--- generate_fake_dataset.py
+++ generate_fake_dataset.py
@@ -12,7 +12,7 @@ import glob
 CT_list = sorted(glob.glob("./data_dir/Task11_SynthRad_task1/t1_ct_norm/*.nii.gz"))
 MR_list = sorted(glob.glob("./data_dir/Task11_SynthRad_task1/t1_mr_norm/*.nii.gz"))
 
-fake_dataset_folder = "./data_dir/Task11_SynthRad_task1_fake/"
+fake_dataset_folder = "./data_dir/Task11_SynthRad_task1_npy/"
 if not os.path.exists(fake_dataset_folder):
     os.mkdir(fake_dataset_folder)
 
@@ -29,10 +29,12 @@ for file_path in MR_list:
     MR_file = nib.load(file_path)
     MR_data = MR_file.get_fdata()
     print(MR_data.shape)
-    new_MR_data = np.zeros(MR_data.shape)
-    new_MR_file = nib.Nifti1Image(new_MR_data, MR_file.affine, MR_file.header)
-    new_save_name = fake_mr_folder + file_name
-    nib.save(new_MR_file, new_save_name)
+    #save the MR_data as npy file
+    np.save(fake_mr_folder + file_name[:-7], MR_data)
+    # new_MR_data = np.zeros(MR_data.shape)
+    # new_MR_file = nib.Nifti1Image(new_MR_data, MR_file.affine, MR_file.header)
+    # new_save_name = fake_mr_folder + file_name
+    # nib.save(new_MR_file, new_save_name)
 
 for file_path in CT_list:
     file_name = os.path.basename(file_path)
@@ -40,7 +42,9 @@ for file_path in CT_list:
     CT_file = nib.load(file_path)
     CT_data = CT_file.get_fdata()
     print(CT_data.shape)
-    new_CT_data = np.zeros(CT_data.shape)
-    new_CT_file = nib.Nifti1Image(new_CT_data, CT_file.affine, CT_file.header)
-    new_save_name = fake_ct_folder + file_name
-    nib.save(new_CT_file, new_save_name)
+    # save the CT_data as npy file
+    np.save(fake_ct_folder + file_name[:-7], CT_data)
+    # new_CT_data = np.zeros(CT_data.shape)
+    # new_CT_file = nib.Nifti1Image(new_CT_data, CT_file.affine, CT_file.header)
+    # new_save_name = fake_ct_folder + file_name
+    # nib.save(new_CT_file, new_save_name)
