e777ecfc1a927781dc34a8e62c45fbdce043b5f0
WeijieChen2017 <weijiechen1994@gmail.com>
2023-04-28 02:15:30 -0500

    Good morning!

--- train_GROWTH_v1.py
+++ train_GROWTH_v1.py
@@ -169,8 +169,9 @@ for training_dict in train_dict["GROWTH_epochs"]:
                 new_state_dict[key][:before_state_dict[key].size()[0], :, :, :, :] = before_state_dict[key]
                 # print("conv.weight", key, new_size, before_size)
             elif "conv.weight" in key or "residual.weight": # conv.weight is a 5d tensor, the first dimension is the number of output channels, the second dimension is the number of input channels
-                new_state_dict[key][:before_state_dict[key].size()[0], :before_state_dict[key].size()[1], :, :, :] = before_state_dict[key]
                 print("conv.weight", key, new_size, before_size)
+                new_state_dict[key][:before_state_dict[key].size()[0], :before_state_dict[key].size()[1], :, :, :] = before_state_dict[key]
+                
             elif "conv.bias" in key or "residual.bias": # conv.bias is a 1d tensor, the first dimension is the number of output channels
                 new_state_dict[key][:before_state_dict[key].size()[0]] = before_state_dict[key]
                 # print("conv.bias", key, new_size, before_size)
