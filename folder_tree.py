b9154697da84f22f5dd1b6196840886e1d8c7da4
WeijieChen2017 <weijiechen1994@gmail.com>
2023-05-04 17:19:49 -0500

    Good morning!

--- folder_tree.py
+++ folder_tree.py
@@ -1,6 +1,6 @@
 import os
 
-def build_tree(folder, file_ext=".nii.gz", level=0):
+def build_tree(folder, file_ext=".nii.gz", level=0, first_n=5, last_n=3):
     contains_target_file = False
     entries = sorted(os.listdir(folder))
     
@@ -11,12 +11,15 @@ def build_tree(folder, file_ext=".nii.gz", level=0):
             break
     
     if contains_target_file:
-        print("  " * level + os.path.basename(folder))
+        folder_name = os.path.basename(folder)
+        if len(folder_name) > first_n + last_n:
+            folder_name = folder_name[:first_n] + "..." + folder_name[-last_n:]
+        print("  " * level + folder_name)
     
     for entry in entries:
         entry_path = os.path.join(folder, entry)
         if os.path.isdir(entry_path):
-            build_tree(entry_path, file_ext, level + 1)
+            build_tree(entry_path, file_ext, level + 1, first_n, last_n)
 
 # Replace 'your_folder_path' with the path to your folder
 your_folder_path = './data_dir/Task1/'
