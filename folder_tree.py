7eb8ed8911ba9aa7a3ababb4244a8e8aaeca860a
WeijieChen2017 <weijiechen1994@gmail.com>
2023-05-04 17:18:14 -0500

    Good morning!

--- folder_tree.py
+++ folder_tree.py
@@ -1,29 +1,23 @@
 import os
-import json
 
-def build_tree(folder, data, category):
-    for entry in sorted(os.listdir(folder)):
+def build_tree(folder, file_ext=".nii.gz", level=0):
+    contains_target_file = False
+    entries = sorted(os.listdir(folder))
+    
+    for entry in entries:
         entry_path = os.path.join(folder, entry)
-        if os.path.isfile(entry_path):
-            for key, file_name in zip(["MR", "CT", "MASK"], ["mr.nii.gz", "ct.nii.gz", "mask.nii.gz"]):
-                if entry.lower() == file_name:
-                    data[key].append(entry_path)
-        elif os.path.isdir(entry_path):
-            if category in entry:
-                build_tree(entry_path, data, category)
+        if os.path.isfile(entry_path) and entry.endswith(file_ext):
+            contains_target_file = True
+            break
+    
+    if contains_target_file:
+        print("  " * level + os.path.basename(folder))
+    
+    for entry in entries:
+        entry_path = os.path.join(folder, entry)
+        if os.path.isdir(entry_path):
+            build_tree(entry_path, file_ext, level + 1)
 
 # Replace 'your_folder_path' with the path to your folder
 your_folder_path = './data_dir/Task1/'
-
-brain_data = {"MR": [], "CT": [], "MASK": []}
-pelvis_data = {"MR": [], "CT": [], "MASK": []}
-
-build_tree(your_folder_path, brain_data, category="brain")
-build_tree(your_folder_path, pelvis_data, category="pelvis")
-
-# Save brain_data and pelvis_data as JSON files
-with open("brain.json", "w") as outfile:
-    json.dump(brain_data, outfile)
-
-with open("pelvis.json", "w") as outfile:
-    json.dump(pelvis_data, outfile)
+build_tree(your_folder_path)
