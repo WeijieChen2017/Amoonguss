27d92f807d82aff665f682b853447def32341192
WeijieChen2017 <weijiechen1994@gmail.com>
2023-05-07 01:59:51 -0500

    Good morning!

--- train_Quaxly_v1.py
+++ train_Quaxly_v1.py
@@ -51,6 +51,7 @@ train_dict["GROWTH_epochs"] = [
 # train_dict["train_epochs"] = train_dict["GROWTH_epochs"][3]["epochs"]
 train_dict["train_epochs"] = 2500
 train_dict["eval_per_epochs"] = 25
+train_dict["save_per_epochs"] = 100
 train_dict["continue_training_epoch"] = 0
 # train_dict["batch"] = train_dict["GROWTH_epochs"][3]["batch"]
 train_dict["start_batch"] = 32
@@ -267,7 +268,7 @@ model = UNet_GROWTH(
 
 torch.backends.cudnn.benchmark = True
 # optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
-optimizer = torch.optim.AdamW(
+optim = torch.optim.AdamW(
     model.parameters(),
     lr = train_dict["opt_lr"],
     betas = train_dict["opt_betas"],
@@ -301,178 +302,122 @@ for idx_epoch_new in range(train_dict["train_epochs"]):
     # training
     model.train()
     n_train_files = len(train_loader)
-    case_loss = np.zeros((n_train_files, 1))
+    curr_iter = n_train_files // batch_size
+    case_loss = np.zeros((curr_iter, 1))
     for step, batch in enumerate(train_loader):
         step += 1
-        # mr, ct, mask = (batch["MR"].cuda(), batch["CT"].cuda(), batch["MASK"].cuda())
-        mr, ct, mask = (batch["MR"], batch["CT"], batch["MASK"])
-        print("step[", step, "]mr", mr.shape, "ct", ct.shape, "mask", mask.shape)
-        if step == 10:
-            exit()
-
-
-
-    for package in [package_train, package_val]:
-
-        file_list = package[0]
-        isTrain = package[1]
-        isVal = package[2]
-        iter_tag = package[3]
-
-        if isTrain:
-            model.train()
-        else:
-            model.eval()
-
-        random.shuffle(file_list)
-        n_file = len(file_list)
-        
-        case_loss = np.zeros((len(file_list), 2))
-
-        # N, C, D, H, W
-        x_data = nib.load(file_list[0]).get_fdata()
-
-        for cnt_file, file_path in enumerate(file_list):
+        mr, ct, mask = (batch["MR"].to(device), batch["CT"].to(device), batch["MASK"].to(device))
+        # mr, ct, mask = (batch["MR"], batch["CT"], batch["MASK"])
+        # print("step[", step, "]mr", mr.shape, "ct", ct.shape, "mask", mask.shape)
+        print(" ===> Train:Epoch[{:03d}]:[{:03d}]/[{:03d}] --->".format(idx_epoch+1, step, curr_iter), end="")
             
-            x_path = file_path
-            y_path = file_path.replace("mr", "ct")
-            file_name = os.path.basename(file_path)
-            print(iter_tag + " ===> Epoch[{:03d}]:[{:03d}]/[{:03d}] --->".format(idx_epoch+1, cnt_file+1, n_file), x_path, "<---", end="")
-            x_file = nib.load(x_path)
-            y_file = nib.load(y_path)
-            x_data = x_file.get_fdata()
-            y_data = y_file.get_fdata()
+        optim.zero_grad()
+        sct = model(mr)
+        masked_sct = sct * mask.unsqueeze(-1)
+        masked_ct = ct * mask.unsqueeze(-1)
+        loss = criterion(masked_ct, masked_sct)
+        # apply the mask on loss
+        masked_loss = loss * mask.unsqueeze(-1)
+        final_loss = torch.sum(masked_loss) / torch.sum(mask)
+        final_loss.backward()
+        optim.step()
+        case_loss[step, 0] = final_loss.item()
+        print("Loss: ", np.sum(case_loss[curr_iter, :]))
+        np.save(train_dict["save_folder"]+"loss/epoch_loss_train_{:04d}.npy".format(idx_epoch+1), case_loss)
 
-            batch_x = np.zeros((train_dict["batch"], 1, train_dict["input_size"][0], train_dict["input_size"][1], train_dict["input_size"][2]))
-            batch_y = np.zeros((train_dict["batch"], 1, train_dict["input_size"][0], train_dict["input_size"][1], train_dict["input_size"][2]))
 
-            for idx_batch in range(train_dict["batch"]):
-                
-                d0_offset = np.random.randint(x_data.shape[0] - train_dict["input_size"][0])
-                d1_offset = np.random.randint(x_data.shape[1] - train_dict["input_size"][1])
-                d2_offset = np.random.randint(x_data.shape[2] - train_dict["input_size"][2])
-
-                x_slice = x_data[d0_offset:d0_offset+train_dict["input_size"][0],
-                                 d1_offset:d1_offset+train_dict["input_size"][1],
-                                 d2_offset:d2_offset+train_dict["input_size"][2]
-                                 ]
-                y_slice = y_data[d0_offset:d0_offset+train_dict["input_size"][0],
-                                 d1_offset:d1_offset+train_dict["input_size"][1],
-                                 d2_offset:d2_offset+train_dict["input_size"][2]
-                                 ]
-                batch_x[idx_batch, 0, :, :, :] = x_slice
-                batch_y[idx_batch, 0, :, :, :] = y_slice
-
-            batch_x = torch.from_numpy(batch_x).float().to(device)
-            batch_y = torch.from_numpy(batch_y).float().to(device)
+    # validation
+    if (idx_epoch+1) % train_dict["val_interval"] == 0:
+        model.eval()
+        n_val_files = len(val_loader)
+        curr_iter = n_val_files // batch_size
+        case_loss = np.zeros((curr_iter, 1))
+        for step, batch in enumerate(val_loader):
+            step += 1
+            mr, ct, mask = (batch["MR"].to(device), batch["CT"].to(device), batch["MASK"].to(device))
+            # mr, ct, mask = (batch["MR"], batch["CT"], batch["MASK"])
+            # print("step[", step, "]mr", mr.shape, "ct", ct.shape, "mask", mask.shape)
+            print(" ===> Validation: Epoch[{:03d}]:[{:03d}]/[{:03d}] --->".format(idx_epoch+1, step, curr_iter), end="")
             
-            if isTrain:
+            with torch.no_grad():
 
-                optim.zero_grad()
-                y_hat = model(batch_x)
-                y_ref = model(batch_x)
-                loss_recon = loss_fnc(y_hat, batch_y)
-                loss_rdrop = loss_doc(y_ref, y_hat)
-                loss = loss_recon + loss_rdrop * train_dict["alpha_dropout_consistency"]
-                loss.backward()
-                optim.step()
-                case_loss[cnt_file, 0] = loss_recon.item()
-                case_loss[cnt_file, 1] = loss_rdrop.item()
-                print("Loss: ", np.sum(case_loss[cnt_file, :]), "Recon: ", loss_recon.item(), "Rdropout: ", loss_rdrop.item())
+                masked_sct = sct * mask.unsqueeze(-1)
+                masked_ct = ct * mask.unsqueeze(-1)
+                loss = criterion(masked_ct, masked_sct)
+                # apply the mask on loss
+                masked_loss = loss * mask.unsqueeze(-1)
+                final_loss = torch.sum(masked_loss) / torch.sum(mask)
+                case_loss[step, 0] = final_loss.item()
+            print("Loss: ", np.sum(case_loss[curr_iter, :]))
+            np.save(train_dict["save_folder"]+"loss/epoch_loss_val_{:04d}.npy".format(idx_epoch+1), case_loss)
 
-            if isVal:
+        best_mae = np.mean(case_loss[:, 0])
+        if best_mae < best_val_loss:
+            best_val_loss = best_mae
+            best_epoch = idx_epoch+1
+            torch.save(model.state_dict(), train_dict["save_folder"]+"model/model_best_{:04d}.pth".format(idx_epoch+1))
+            torch.save(optim.state_dict(), train_dict["save_folder"]+"model/optim_best_{:04d}.pth".format(idx_epoch+1))
+            print("Best model saved at epoch {:03d} with MAE {:03f}".format(best_epoch, best_val_loss*4024))
 
-                with torch.no_grad():
-                    y_hat = model(batch_x)
-                    y_ref = model(batch_x)
-                    loss_recon = loss_fnc(y_hat, batch_y)
-                    loss_rdrop = loss_doc(y_ref, y_hat)
-                    loss = loss_recon + loss_rdrop * train_dict["alpha_dropout_consistency"]
-
-                case_loss[cnt_file, 0] = loss_recon.item()
-                case_loss[cnt_file, 1] = loss_rdrop.item()
-                print("Loss: ", np.sum(case_loss[cnt_file, :]), "Recon: ", loss_recon.item(), "Rdropout: ", loss_rdrop.item())
-
-        epoch_loss_recon = np.mean(case_loss[:, 0])
-        epoch_loss_rdrop = np.mean(case_loss[:, 1])
-        # epoch_loss = np.mean(case_loss)
-        epoch_loss = epoch_loss_recon
-        print(iter_tag + " ===>===> Epoch[{:03d}]: ".format(idx_epoch+1), end='')
-        print("Loss: ", epoch_loss, "Recon: ", epoch_loss_recon, "Rdropout: ", epoch_loss_rdrop)
-        np.save(train_dict["save_folder"]+"loss/epoch_loss_"+iter_tag+"_{:03d}.npy".format(idx_epoch+1), case_loss)
-
-        if isVal:
-            # np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_".format(idx_epoch+1, file_name)+iter_tag+"_xf.npy", batch_xf.cpu().detach().numpy())
-            # np.save(train_dict["save_folder"]+"npy/Epoch[{:03d}]_Case[{}]_".format(idx_epoch+1, file_name)+iter_tag+"_fmap.npy", batch_fmap.cpu().detach().numpy())
-            torch.save(model.state_dict(), train_dict["save_folder"]+"model_curr.pth")
-            torch.save(optim.state_dict(), train_dict["save_folder"]+"optim_curr.pth")
-            
-            if epoch_loss < best_val_loss:
-                # save the best model
-                torch.save(model.state_dict(), train_dict["save_folder"]+"model_best_{:03d}.pth".format(idx_epoch + 1))
-                torch.save(optim.state_dict(), train_dict["save_folder"]+"optim_best_{:03d}.pth".format(idx_epoch + 1))
-                print("Checkpoint saved at Epoch {:03d}".format(idx_epoch + 1))
-                best_val_loss = epoch_loss
-
-        # del batch_x, batch_y
-        # gc.collect()
-        # torch.cuda.empty_cache()
+    # save the model every train_dict["save_per_epochs"] epochs
+    if (idx_epoch+1) % train_dict["save_per_epochs"] == 0:
+        torch.save(model.state_dict(), train_dict["save_folder"]+"model/model_{:04d}.pth".format(idx_epoch+1))
+        torch.save(optim.state_dict(), train_dict["save_folder"]+"model/optim_{:04d}.pth".format(idx_epoch+1))
+        print("Model saved at epoch {:03d}".format(idx_epoch+1))
+    
+# def validation(epoch_iterator_val):
+#     model.eval()
+#     with torch.no_grad():
+#         for batch in epoch_iterator_val:
+#             val_inputs, val_labels = (batch["image"].cuda(), batch["label"].cuda())
+#             val_outputs = sliding_window_inference(val_inputs, (96, 96, 96), 4, model)
+#             val_labels_list = decollate_batch(val_labels)
+#             val_labels_convert = [post_label(val_label_tensor) for val_label_tensor in val_labels_list]
+#             val_outputs_list = decollate_batch(val_outputs)
+#             val_output_convert = [post_pred(val_pred_tensor) for val_pred_tensor in val_outputs_list]
+#             dice_metric(y_pred=val_output_convert, y=val_labels_convert)
+#             epoch_iterator_val.set_description("Validate (%d / %d Steps)" % (global_step, 10.0))
+#         mean_dice_val = dice_metric.aggregate().item()
+#         dice_metric.reset()
+#     return mean_dice_val
 
 
-
-def validation(epoch_iterator_val):
-    model.eval()
-    with torch.no_grad():
-        for batch in epoch_iterator_val:
-            val_inputs, val_labels = (batch["image"].cuda(), batch["label"].cuda())
-            val_outputs = sliding_window_inference(val_inputs, (96, 96, 96), 4, model)
-            val_labels_list = decollate_batch(val_labels)
-            val_labels_convert = [post_label(val_label_tensor) for val_label_tensor in val_labels_list]
-            val_outputs_list = decollate_batch(val_outputs)
-            val_output_convert = [post_pred(val_pred_tensor) for val_pred_tensor in val_outputs_list]
-            dice_metric(y_pred=val_output_convert, y=val_labels_convert)
-            epoch_iterator_val.set_description("Validate (%d / %d Steps)" % (global_step, 10.0))
-        mean_dice_val = dice_metric.aggregate().item()
-        dice_metric.reset()
-    return mean_dice_val
-
-
-def train(global_step, train_loader, dice_val_best, global_step_best):
-    model.train()
-    epoch_loss = 0
-    step = 0
-    epoch_iterator = tqdm(train_loader, desc="Training (X / X Steps) (loss=X.X)", dynamic_ncols=True)
-    for step, batch in enumerate(epoch_iterator):
-        step += 1
-        x, y = (batch["image"].cuda(), batch["label"].cuda())
-        logit_map = model(x)
-        loss = loss_function(logit_map, y)
-        loss.backward()
-        epoch_loss += loss.item()
-        optimizer.step()
-        optimizer.zero_grad()
-        epoch_iterator.set_description("Training (%d / %d Steps) (loss=%2.5f)" % (global_step, max_iterations, loss))
-        if (global_step % eval_num == 0 and global_step != 0) or global_step == max_iterations:
-            epoch_iterator_val = tqdm(val_loader, desc="Validate (X / X Steps) (dice=X.X)", dynamic_ncols=True)
-            dice_val = validation(epoch_iterator_val)
-            epoch_loss /= step
-            epoch_loss_values.append(epoch_loss)
-            metric_values.append(dice_val)
-            if dice_val > dice_val_best:
-                dice_val_best = dice_val
-                global_step_best = global_step
-                torch.save(model.state_dict(), os.path.join(root_dir, "best_metric_model.pth"))
-                print(
-                    "Model Was Saved ! Current Best Avg. Dice: {} Current Avg. Dice: {}".format(dice_val_best, dice_val)
-                )
-            else:
-                print(
-                    "Model Was Not Saved ! Current Best Avg. Dice: {} Current Avg. Dice: {}".format(
-                        dice_val_best, dice_val
-                    )
-                )
-        global_step += 1
-    return global_step, dice_val_best, global_step_best
+# def train(global_step, train_loader, dice_val_best, global_step_best):
+#     model.train()
+#     epoch_loss = 0
+#     step = 0
+#     epoch_iterator = tqdm(train_loader, desc="Training (X / X Steps) (loss=X.X)", dynamic_ncols=True)
+#     for step, batch in enumerate(epoch_iterator):
+#         step += 1
+#         x, y = (batch["image"].cuda(), batch["label"].cuda())
+#         logit_map = model(x)
+#         loss = loss_function(logit_map, y)
+#         loss.backward()
+#         epoch_loss += loss.item()
+#         optimizer.step()
+#         optimizer.zero_grad()
+#         epoch_iterator.set_description("Training (%d / %d Steps) (loss=%2.5f)" % (global_step, max_iterations, loss))
+#         if (global_step % eval_num == 0 and global_step != 0) or global_step == max_iterations:
+#             epoch_iterator_val = tqdm(val_loader, desc="Validate (X / X Steps) (dice=X.X)", dynamic_ncols=True)
+#             dice_val = validation(epoch_iterator_val)
+#             epoch_loss /= step
+#             epoch_loss_values.append(epoch_loss)
+#             metric_values.append(dice_val)
+#             if dice_val > dice_val_best:
+#                 dice_val_best = dice_val
+#                 global_step_best = global_step
+#                 torch.save(model.state_dict(), os.path.join(root_dir, "best_metric_model.pth"))
+#                 print(
+#                     "Model Was Saved ! Current Best Avg. Dice: {} Current Avg. Dice: {}".format(dice_val_best, dice_val)
+#                 )
+#             else:
+#                 print(
+#                     "Model Was Not Saved ! Current Best Avg. Dice: {} Current Avg. Dice: {}".format(
+#                         dice_val_best, dice_val
+#                     )
+#                 )
+#         global_step += 1
+#     return global_step, dice_val_best, global_step_best
 
 
 
