3541d3eb22ae4bd321430211556ca542257d584a
WeijieChen2017 <weijiechen1994@gmail.com>
2023-05-08 00:23:14 -0500

    Good morning!

--- test_Quaxly_v2.py
+++ test_Quaxly_v2.py
@@ -54,7 +54,7 @@ unet_dict["spatial_dims"] = 3
 unet_dict["in_channels"] = 1
 unet_dict["out_channels"] = 1
 unet_dict["channels"] = (16, 32, 64, 128, 256)
-unet_dict["strides"] = (2, 2, 2)
+unet_dict["strides"] = (2, 2, 2, 2)
 unet_dict["num_res_units"] = 4
 
 train_dict["model_para"] = unet_dict
