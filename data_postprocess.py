5d130ee3cf4bde664837621c7e5ce6d7f5577550
WeijieChen2017 <weijiechen1994@gmail.com>
2023-05-15 23:59:51 -0500

    Good morning!

--- data_postprocess.py
+++ data_postprocess.py
@@ -2,7 +2,7 @@ import numpy
 import glob
 import os
 
-# the ct file is in ./data_dir/Task_1/brain/1BA001/ct.nii.gz
+# the ct file is in ./data_dir/Task1/brain/1BA001/ct.nii.gz
 # the predicted sct is in ./project_dir/Quaxly_brain_v3a/ensemble_last/brain_1BA001_sct.nii.gz
 
 sct_file_list = sorted(glob.glob("./project_dir/Quaxly_brain_v3a/ensemble_last/*_sct.nii.gz"))
@@ -10,8 +10,8 @@ for sct_path in sct_file_list:
     organ = sct_path.split("/")[-1].split("_")[0]
     case_id = sct_path.split("/")[-1].split("_")[1]
     # ct_path = "./data_dir/Task_1/"+organ+"/"+case_id+"/ct.nii.gz"
-    sct_dst = "./data_dir/Task_1/"+organ+"/"+case_id+"/sct_v3a_last.nii.gz"
-    std_dst = "./data_dir/Task_1/"+organ+"/"+case_id+"/std_v3a_last.nii.gz"
+    sct_dst = "./data_dir/Task1/"+organ+"/"+case_id+"/sct_v3a_last.nii.gz"
+    std_dst = "./data_dir/Task1/"+organ+"/"+case_id+"/std_v3a_last.nii.gz"
     std_path = "./project_dir/Quaxly_brain_v3a/ensemble_last/"+organ+"_"+case_id+"_std.nii.gz"
     os.system("cp "+sct_path+" "+sct_dst)
     print("Copied: ", sct_path, " to ", sct_dst)
