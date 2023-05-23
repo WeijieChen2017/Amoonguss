78cac6e190d782bd4529427c81905e33c4113280
WeijieChen2017 <weijiechen1994@gmail.com>
2023-05-23 13:59:08 -0500

    Good morning!

--- train_Quaxly_v3b.py
+++ train_Quaxly_v3b.py
@@ -8,11 +8,11 @@ import time
 import numpy as np
 
 model_list = [
-    ["Quaxly_brain_v3b", [7], 912, 6, 0, "brain"], # dgx1
+    ["Quaxly_brain_v3b", [7], 912, 6, 0, "brain"], # ongoing
     ["Quaxly_brain_v3b", [4], 912, 6, 1, "brain"], # ongoing
     ["Quaxly_brain_v3b", [4], 912, 6, 2, "brain"], # ongoing
     ["Quaxly_brain_v3b", [5], 912, 6, 3, "brain"], # ongoing
-    ["Quaxly_brain_v3b", [5], 912, 6, 4, "brain"], # dgx1
+    ["Quaxly_brain_v3b", [4], 912, 6, 4, "brain"], # dgx1
     ["Quaxly_brain_v3b", [5], 912, 6, 5, "brain"], # dgx1
     # ["Quaxly_task1_v4", [7], 912, 6, 0, "task1"], #dgx2 runing
     # ["Quaxly_task1_v4", [7], 912, 6, 1, "task1"], #dgx2 runing
