a4151e6f5a0d0f4ec3ac29c82d3919e5a15878c8
WeijieChen2017 <weijiechen1994@gmail.com>
2023-05-22 21:39:15 -0500

    Good morning!

--- test_Quaxly_v3b.py
+++ test_Quaxly_v3b.py
@@ -91,96 +91,96 @@ for idx_fold in [1,2,3]:
         val_files = datasets["validation"]
 
     # test for the best model
-    best_model = root_dir + "model/fold_{:02d}_model_best.pth".format(curr_fold)
-    print("best_model: ", best_model)
-    best_model = torch.load(best_model)
+    # best_model = root_dir + "model/fold_{:02d}_model_best.pth".format(curr_fold)
+    # print("best_model: ", best_model)
+    # best_model = torch.load(best_model)
 
-    model = UNet_Quaxly( 
-        spatial_dims=unet_dict["spatial_dims"],
-        in_channels=unet_dict["in_channels"],
-        out_channels=unet_dict["out_channels"],
-        channels=unet_dict["channels"],
-        strides=unet_dict["strides"],
-        num_res_units=unet_dict["num_res_units"],
-        )
-    model.load_state_dict(best_model)
-    model.eval()
-    model.to(device)
-    n_val_files = len(val_files)
-    mae_val = np.zeros(n_val_files)
+    # model = UNet_Quaxly( 
+    #     spatial_dims=unet_dict["spatial_dims"],
+    #     in_channels=unet_dict["in_channels"],
+    #     out_channels=unet_dict["out_channels"],
+    #     channels=unet_dict["channels"],
+    #     strides=unet_dict["strides"],
+    #     num_res_units=unet_dict["num_res_units"],
+    #     )
+    # model.load_state_dict(best_model)
+    # model.eval()
+    # model.to(device)
+    # n_val_files = len(val_files)
+    # mae_val = np.zeros(n_val_files)
 
-    for idx_case, val_case in enumerate(val_files):
-        print("[{:03d}]/[{:03d}] Processing: ".format(idx_case+1, n_val_files), end="<--->")
-        mr_path = val_case["MR"]
-        ct_path = val_case["CT"]
-        v3a_last_path = val_case["v3a_last"]
-        # mr, ct, mask = (batch["MR"], batch["CT"], batch["MASK"])
-        # print("step[", step, "]mr", mr.shape, "ct", ct.shape, "mask", mask.shape)
-        mask_path = val_case["MASK"]
-        mr_file = nib.load(mr_path)
-        ct_file = nib.load(ct_path)
-        v3a_last_file = nib.load(v3a_last_path)
-        mask_file = nib.load(mask_path)
-        # mr_path is like ./data_dir/Task_1/brain/1BA001/mr.nii.gz
-        organ_case = mr_path.split("/")[-3]+"_"+mr_path.split("/")[-2]
-        print("Loaded: ", mr_path, end="<--->")
+    # for idx_case, val_case in enumerate(val_files):
+    #     print("[{:03d}]/[{:03d}] Processing: ".format(idx_case+1, n_val_files), end="<--->")
+    #     mr_path = val_case["MR"]
+    #     ct_path = val_case["CT"]
+    #     v3a_last_path = val_case["v3a_last"]
+    #     # mr, ct, mask = (batch["MR"], batch["CT"], batch["MASK"])
+    #     # print("step[", step, "]mr", mr.shape, "ct", ct.shape, "mask", mask.shape)
+    #     mask_path = val_case["MASK"]
+    #     mr_file = nib.load(mr_path)
+    #     ct_file = nib.load(ct_path)
+    #     v3a_last_file = nib.load(v3a_last_path)
+    #     mask_file = nib.load(mask_path)
+    #     # mr_path is like ./data_dir/Task_1/brain/1BA001/mr.nii.gz
+    #     organ_case = mr_path.split("/")[-3]+"_"+mr_path.split("/")[-2]
+    #     print("Loaded: ", mr_path, end="<--->")
 
-        mr_data = mr_file.get_fdata()
-        ct_data = ct_file.get_fdata()
-        mask_data = mask_file.get_fdata()
-        v3a_last_data = v3a_last_file.get_fdata()
+    #     mr_data = mr_file.get_fdata()
+    #     ct_data = ct_file.get_fdata()
+    #     mask_data = mask_file.get_fdata()
+    #     v3a_last_data = v3a_last_file.get_fdata()
 
-        mr_data = mr_data / 3000
-        v3a_last_data = (v3a_last_data + 1024) / 4024
-        mr_data = np.expand_dims(mr_data, (0,1))
-        v3a_last_data = np.expand_dims(v3a_last_data, (0,1))
-        mr_data = torch.from_numpy(mr_data).float()
-        v3a_last_data = torch.from_numpy(v3a_last_data).float()
-        input_mr_v3a_last = torch.concat((mr_data, v3a_last_data), dim=1).to(device)
-        # input_data = np.expand_dims(mr_data, (0,1))
-        # input_data = torch.from_numpy(input_data).float().to(device)
+    #     mr_data = mr_data / 3000
+    #     v3a_last_data = (v3a_last_data + 1024) / 4024
+    #     mr_data = np.expand_dims(mr_data, (0,1))
+    #     v3a_last_data = np.expand_dims(v3a_last_data, (0,1))
+    #     mr_data = torch.from_numpy(mr_data).float()
+    #     v3a_last_data = torch.from_numpy(v3a_last_data).float()
+    #     input_mr_v3a_last = torch.concat((mr_data, v3a_last_data), dim=1).to(device)
+    #     # input_data = np.expand_dims(mr_data, (0,1))
+    #     # input_data = torch.from_numpy(input_data).float().to(device)
 
-        sct = sliding_window_inference(
-        inputs = input_mr_v3a_last, 
-        roi_size = train_dict["input_size"][0], 
-        sw_batch_size = 32, 
-        predictor = model,
-        overlap=1/4, 
-        mode="gaussian", 
-        sigma_scale=1/4, 
-        padding_mode="constant", 
-        cval=0.0, 
-        sw_device=device, 
-        device=device,
-        # is_deep_supervision = False,
-        )
+    #     sct = sliding_window_inference(
+    #     inputs = input_mr_v3a_last, 
+    #     roi_size = train_dict["input_size"][0], 
+    #     sw_batch_size = 32, 
+    #     predictor = model,
+    #     overlap=1/4, 
+    #     mode="gaussian", 
+    #     sigma_scale=1/4, 
+    #     padding_mode="constant", 
+    #     cval=0.0, 
+    #     sw_device=device, 
+    #     device=device,
+    #     # is_deep_supervision = False,
+    #     )
 
-        sct = np.squeeze(sct.cpu().detach().numpy()) # 0->1
-        sct = sct * mask_data # 0->1
-        sct = sct * 4024 - 1024 # -1024->3000
-        ct = (ct_data + 1024)/4024 # 0->1
-        ct = ct * mask_data
-        ct = ct * 4024 - 1024 # -1024 -> 3000
-        sct = np.clip(sct, -1024, 3000)
-        ct = np.clip(ct, -1024, 3000)
+    #     sct = np.squeeze(sct.cpu().detach().numpy()) # 0->1
+    #     sct = sct * mask_data # 0->1
+    #     sct = sct * 4024 - 1024 # -1024->3000
+    #     ct = (ct_data + 1024)/4024 # 0->1
+    #     ct = ct * mask_data
+    #     ct = ct * 4024 - 1024 # -1024 -> 3000
+    #     sct = np.clip(sct, -1024, 3000)
+    #     ct = np.clip(ct, -1024, 3000)
 
 
-        masked_mae = np.sum(np.abs(ct - sct)) / np.sum(mask_data)
-        print("Masked MAE: ", masked_mae)
-        # whole_mae = np.mean(np.abs(ct - sct))
-        # print("Whole MAE: ", whole_mae)
-        sct_file = nib.Nifti1Image(sct, ct_file.affine, ct_file.header)
-        sct_savename = train_dict["save_folder"]+"eval_best/"+organ_case+"_sct.nii.gz"
-        nib.save(sct_file, sct_savename)
-        print("Saved: ", sct_savename)
-        np.save(train_dict["save_folder"]+"eval_best/"+organ_case+"_mae.npy", masked_mae)
-        mae_val[idx_case] = masked_mae
-    print("Mean MAE: ", np.mean(mae_val))
+    #     masked_mae = np.sum(np.abs(ct - sct)) / np.sum(mask_data)
+    #     print("Masked MAE: ", masked_mae)
+    #     # whole_mae = np.mean(np.abs(ct - sct))
+    #     # print("Whole MAE: ", whole_mae)
+    #     sct_file = nib.Nifti1Image(sct, ct_file.affine, ct_file.header)
+    #     sct_savename = train_dict["save_folder"]+"eval_best/"+organ_case+"_sct.nii.gz"
+    #     nib.save(sct_file, sct_savename)
+    #     print("Saved: ", sct_savename)
+    #     np.save(train_dict["save_folder"]+"eval_best/"+organ_case+"_mae.npy", masked_mae)
+    #     mae_val[idx_case] = masked_mae
+    # print("Mean MAE: ", np.mean(mae_val))
 
     # test for the last model
     # model_list = sorted(glob.glob(root_dir + "model/fold_{:02d}_model_*000.pth".format(curr_fold)))
 
-    last_model = root_dir + "model/fold_{:02d}_model_10000.pth".format(curr_fold)
+    last_model = root_dir + "model/fold_{:02d}_model_6000.pth".format(curr_fold)
     print("last_model: ", last_model)
     last_model = torch.load(last_model)
 
