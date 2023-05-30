c9f8a0b2afdc3938a3381db7b4bf19313ab472e6
WeijieChen2017 <weijiechen1994@gmail.com>
2023-05-29 22:17:56 -0500

    Good morning!

--- cal_loss_v3a.py
+++ cal_loss_v3a.py
@@ -3,5 +3,9 @@ import os
 import numpy as np
 import nibabel as nib
 
-X_folder = "./project_dir/Quaxly_brain_v3a/eval_best/"
-Y_folder = 
+data_dir = "./data_dir/Task1/brain/"
+folder_list = sorted(glob.glob(data_dir+"*/mr.nii.gz"))
+for folder_path in folder_list:
+    print(folder_list)
+    case_id = folder_path.split("/")[-2]
+    print(case_id)
