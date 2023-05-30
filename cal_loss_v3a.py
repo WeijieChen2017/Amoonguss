74b4272c5890a049b42dcbecc3b6dee145547e2f
WeijieChen2017 <weijiechen1994@gmail.com>
2023-05-29 22:19:25 -0500

    Good morning!

--- cal_loss_v3a.py
+++ cal_loss_v3a.py
@@ -6,6 +6,5 @@ import os
 data_dir = "./data_dir/Task1/brain/"
 folder_list = sorted(glob.glob(data_dir+"*/mr.nii.gz"))
 for folder_path in folder_list:
-    print(folder_list)
     case_id = folder_path.split("/")[-2]
     print(case_id)
