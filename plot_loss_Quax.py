0b0b0e2fc28e31105eed16fc0a9d307b6a0a5d44
WeijieChen2017 <weijiechen1994@gmail.com>
2023-05-29 19:30:45 -0500

    Good morning!

---
+++ plot_loss_Quax.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+import copy
+import os
+
+n_fold = 6
+folder = "./project_dir/Quaxly_brain_v3a/"
+
+plot_data = []
+fold_hub = []
+for idx_fold in range(n_fold):
+    train_npy_list = sorted(glob.glob(folder+"loss/fold_{:02d}_train_*.npy".format(idx_fold)))
+    val_npy_list = sorted(glob.glob(folder+"loss/fold_{:02d}_val_*.npy".format(idx_fold)))
+    for npy_path in train_npy_list:
+        print(npy_path)
+        data = np.load(npy_path)
+        print(data)
+        print(data.shape)
+    
+
+# # npy_list = sorted(glob.glob(folder+"loss/epoch_loss_*.npy"))
+# for npy_path in npy_list:
+#     print(npy_path)
+#     stage_name = os.path.basename(npy_path)
+#     stage_name = stage_name.split("_")[2]
+#     print(stage_name)
+#     if not stage_name in stage_hub:
+#         stage_hub.append(stage_name)
+
+# loss = np.zeros((n_epoch))
+# plot_target = []
+# for stage_name in stage_hub:
+#     current_package = [stage_name]
+#     for idx in range(n_epoch):
+#         num = "{:03d}".format(idx+1)
+#         name = folder+"loss/epoch_loss_{}_{}.npy".format(stage_name, num)
+#         data = np.load(name)
+#         loss[idx] = np.mean(data)
+#     current_package.append(copy.deepcopy(loss))
+#     plot_target.append(current_package)
+
+# legend_list = []
+# plt.figure(figsize=(9,6), dpi=300)
+# for package in plot_target:
+#     loss_array = package[1]
+#     loss_tag = package[0]
+#     legend_list.append(loss_tag)
+#     print(loss_tag, np.mean(loss_array))
+#     plt.plot(range(n_epoch), loss_array)
+
+# plt.xlabel("epoch")
+# plt.ylabel("loss")
+# plt.yscale("log")
+# plt.legend(legend_list)
+# plt.title("Training curve of "+folder.split("/")[-2])
+
+# plt.savefig(folder + "loss_{}.jpg".format(n_epoch))
