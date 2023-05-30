3ffeb6564ebd5a0efa6ffbb2ffeac0146a379d9d
WeijieChen2017 <weijiechen1994@gmail.com>
2023-05-29 19:43:57 -0500

    Good morning!

--- plot_loss_Quax.py
+++ plot_loss_Quax.py
@@ -9,8 +9,9 @@ n_fold = 6
 folder = "./project_dir/Quaxly_brain_v3a/"
 
 fold_hub = {}
+val_epoch = []
 for idx_fold in range(n_fold):
-    fold_hub[idx_fold] = {"train":[], "val":[]}
+    fold_hub[idx_fold] = {"train":[], "val":{}}
     train_npy_list = sorted(glob.glob(folder+"loss/fold_{:02d}_train_*.npy".format(idx_fold)))
     val_npy_list = sorted(glob.glob(folder+"loss/fold_{:02d}_val_*.npy".format(idx_fold)))
     for npy_path in train_npy_list:
@@ -18,12 +19,20 @@ for idx_fold in range(n_fold):
         data = np.load(npy_path)
         MAE_HU = np.mean(data) * 4024 - 1024
         fold_hub[idx_fold]["train"].append(MAE_HU)
+    print("Fold {:02d}:".format(idx_fold), "Epoch_train:", len(fold_hub[idx_fold]["train"]))
+
+    min_epoch = 0
     for npy_path in val_npy_list:
         # print(npy_path)
         data = np.load(npy_path)
+        # fold_03_val_9900.npy
+        epoch = int(os.path.basename(npy_path).split("_")[3])
+        min_epoch = epoch
+        if not epoch in val_epoch:
+            val_epoch.append(epoch)
         MAE_HU = np.mean(data) * 4024 - 1024
-        fold_hub[idx_fold]["val"].append(MAE_HU)
-    print("Fold {:02d}:".format(idx_fold), "Epoch_train:", len(fold_hub[idx_fold]["train"]), "Epoch_val:", len(fold_hub[idx_fold]["val"]))
+        fold_hub[idx_fold]["val"][epoch] = MAE_HU
+    print("Fold {:02d}:".format(idx_fold), "Epoch_val:", max(min_epoch))
 
 timestamp = time.strftime("%Y%m%d_%H%M%S", time.localtime())
 savename = folder + "overall_loss_{:02d}_{}.jpg".format(n_fold, timestamp)
