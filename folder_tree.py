02621bd6cd2986aac76d2061a34af675f5c5fe23
WeijieChen2017 <weijiechen1994@gmail.com>
2023-05-31 14:24:00 -0500

    Good morning!

--- folder_tree.py
+++ folder_tree.py
@@ -8,7 +8,7 @@ def build_tree(folder, data):
         entry_path = os.path.join(folder, entry)
         if os.path.isfile(entry_path) and entry.lower().endswith(".nii.gz"):
             category = "brain" if "brain" in entry_path.lower() else "pelvis"
-            for key, file_name in zip(["MR", "CT", "MASK"], ["mr.nii.gz", "ct.nii.gz", "mask.nii.gz"]):
+            for key, file_name in zip(["MR", "CT", "MASK_MR"], ["mr.nii.gz", "ct.nii.gz", "mask_mri_th50.nii.gz",]):
                 if entry.lower() == file_name:
                     data[category][key].append(entry_path)
         elif os.path.isdir(entry_path):
@@ -19,14 +19,14 @@ def build_tree(folder, data):
 your_folder_path = './data_dir/Task1/'
 
 data = {
-    "brain": {"MR": [], "CT": [], "MASK": []},
-    "pelvis": {"MR": [], "CT": [], "MASK": []}
+    "brain": {"MR": [], "CT": [], "MASK_MR": []},
+    "pelvis": {"MR": [], "CT": [], "MASK_MR": []}
 }
 
 build_tree(your_folder_path, data)
 
 # create a "task1" key in data dict to combine brain and pelvis data with the same key
-data["task1"] = {key: data["brain"][key] + data["pelvis"][key] for key in ["MR", "CT", "MASK"]}
+data["task1"] = {key: data["brain"][key] + data["pelvis"][key] for key in ["MR", "CT", "MASK_MR"]}
 
 # Save brain_data and pelvis_data as JSON files
 # with open(your_folder_path+"brain.json", "w") as outfile:
@@ -35,5 +35,5 @@ data["task1"] = {key: data["brain"][key] + data["pelvis"][key] for key in ["MR",
 # with open(your_folder_path+"pelvis.json", "w") as outfile:
 #     json.dump(data["pelvis"], outfile)
 
-with open(your_folder_path+"task1.json", "w") as outfile:
+with open(your_folder_path+"task1_mri_mask.json", "w") as outfile:
     json.dump(data["task1"], outfile)
--- generate_mask.py
+++ generate_mask.py
@@ -14,12 +14,12 @@ import glob
 data_folder = "./data_dir/Task1/brain/"
 mri_paths_list = sorted(glob.glob(data_folder+"*/mr.nii.gz"))
 
-def generate_mask(mri_data, threshold, dilation_radius=3, blur_radius=2):
+def generate_mask(mri_data, value_threshold, guassian_threshold, dilation_radius=3, blur_radius=2):
     # Load the MRI image 
     mri_img = mri_data
 
     # Apply threshold: create a binary mask where values greater than threshold are set to 1
-    binary_mask = np.where(mri_img > threshold, 1, 0)
+    binary_mask = np.where(mri_img > value_threshold, 1, 0)
 
     # For each axial slice, fill the holes
     filled_mask = np.zeros_like(binary_mask)
@@ -33,7 +33,7 @@ def generate_mask(mri_data, threshold, dilation_radius=3, blur_radius=2):
     blurred_mask = gaussian_filter(np.float32(dilated_mask), sigma=blur_radius)
 
     # Re-threshold to keep the mask binary
-    final_mask = np.where(blurred_mask > 0.9, 1, 0)  # Assuming values in the mask are 0 or 1
+    final_mask = np.where(blurred_mask > guassian_threshold, 1, 0)  # Assuming values in the mask are 0 or 1
 
     return final_mask
 
@@ -41,7 +41,11 @@ for mri_path in mri_paths_list:
     print("mri_path: ", mri_path)
     mri_file = nib.load(mri_path)
     mri_data = mri_file.get_fdata()
-    mri_mask = generate_mask(mri_data, 50)
+    mri_mask = generate_mask(
+        mri_data=mri_data, 
+        value_threshold=50,
+        guassian_threshold=0.9,
+    )
     mask_file = nib.Nifti1Image(mri_mask, mri_file.affine, mri_file.header)
     mask_path = mri_path.replace("mr.nii.gz", "mask_mri_th50.nii.gz")
     nib.save(mask_file, mask_path)
