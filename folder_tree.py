3437d4047fe219b8b375848a9ba55d94306b6419
WeijieChen2017 <weijiechen1994@gmail.com>
2023-06-05 16:30:08 -0500

    Good morning!

--- folder_tree.py
+++ folder_tree.py
@@ -8,7 +8,7 @@ def build_tree(folder, data):
         entry_path = os.path.join(folder, entry)
         if os.path.isfile(entry_path) and entry.lower().endswith(".nii.gz"):
             category = "brain" if "brain" in entry_path.lower() else "pelvis"
-            for key, file_name in zip(["MR", "CT", "MASK_MR"], ["mr.nii.gz", "ct.nii.gz", "mask_mri_th50.nii.gz",]):
+            for key, file_name in zip(["MR", "CT", "MASK_MR"], ["mr.nii.gz", "ct.nii.gz", "mask_mri_th60.nii.gz",]):
                 if entry.lower() == file_name:
                     data[category][key].append(entry_path)
         elif os.path.isdir(entry_path):
