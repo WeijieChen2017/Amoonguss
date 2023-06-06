ae7dfd158d33a370e5f2ca2d844c96f2c6329c88
WeijieChen2017 <weijiechen1994@gmail.com>
2023-06-06 01:26:30 -0500

    Good morning!

--- train_Quaxly_v3_mri_mask.py
+++ train_Quaxly_v3_mri_mask.py
@@ -172,7 +172,7 @@ train_transforms = Compose(
         # ),
         RandSpatialCropSamplesd(
             keys=["MR", "CT", "MASK_MR"],
-            num_samples = 4, 
+            num_samples = 32, 
             roi_size=train_dict["input_size"], 
             random_size=False,
         ),
